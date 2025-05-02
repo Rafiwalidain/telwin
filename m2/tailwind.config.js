05d42c22d32dee69f552b867e88f1577ae76ec5d
Muhammad Rafi Walidain <rafi.walidain81@gmail.com>
2025-05-02 15:13:28 +0700

    keren rek

--- m2/tailwind.config.js
+++ m2/tailwind.config.js
@@ -6,3 +6,32 @@ module.exports = {
   },
   plugins: [],
 };
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        13: "3.25rem",
+        15: "3.75rem",
+        128: "32rem",
+        144: "36rem",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        bubbleGum: "#ff77e9",
+        bermuda: "#78dcca",
+      },
+    },
+  },
+};
